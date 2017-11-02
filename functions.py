df15490c17b9a1986c8ccd05bc499cd8c9a6a62a
Sanja Žerovnik <sanja.z@amis.net>
2017-11-02 18:38:26 +0100

    New stuff, loops, lists, guess number, to do list, mile to km

---
+++ functions.py
@@ -0,0 +1,19 @@
+def sum_num(a, b):
+    """
+    :param a: first number of sum, int
+    :param b: second number of sum
+    :return:
+    """
+    c = a + b
+
+    return c
+
+def generate_list(a, b, c, d):
+
+    new_list = []
+    new_list.append(a)
+    new_list.append(b)
+    new_list.append(c)
+    new_list.append(d)
+
+    return new_list
